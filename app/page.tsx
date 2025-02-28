c28aad119813324acef3e1b8f9565de30cc188db
tahaygun <tahaygun@gmail.com>
2025-02-28 17:06:14 +0100

    Add total nutrition summary for multi-item meals

--- app/page.tsx
+++ app/page.tsx
@@ -383,6 +383,27 @@ export default function Home() {
                         </div>
                       </div>
                     ))}
+                    {meal.items.length > 1 && (
+                      <div className='bg-gray-700/50 p-2 rounded mt-2 border-t border-gray-600'>
+                        <div className='grid grid-cols-5 gap-2 text-xs text-gray-300'>
+                          <div>
+                            <span className='text-gray-400'>Cal</span> {meal.items.reduce((sum, item) => sum + item.nutrition.calories, 0)}
+                          </div>
+                          <div>
+                            <span className='text-gray-400'>P</span> {meal.items.reduce((sum, item) => sum + item.nutrition.protein, 0)}g
+                          </div>
+                          <div>
+                            <span className='text-gray-400'>C</span> {meal.items.reduce((sum, item) => sum + item.nutrition.carbs, 0)}g
+                          </div>
+                          <div>
+                            <span className='text-gray-400'>F</span> {meal.items.reduce((sum, item) => sum + item.nutrition.fat, 0)}g
+                          </div>
+                          <div>
+                            <span className='text-gray-400'>Fb</span> {meal.items.reduce((sum, item) => sum + item.nutrition.fiber, 0)}g
+                          </div>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
               ))}
